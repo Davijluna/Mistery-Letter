092d96e41e96cd977e3fc9c5c4946a70ea146bbf
davi <davijluna@gmail.com>
2023-05-05 00:02:15 -0300

    feat: adicionando arquivo e fazendo limpeza para disponibilizar projeto no repositorio pessoal

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-const texto = document.querySelector('#carta-texto');
+const texto = document.querySelector('#carta-texto').value;
 const botton = document.querySelector('#criar-carta');
 
 botton.addEventListener('click', () => {
   const elementoPai = document.querySelector('#carta-gerada');
-  const titulo = document.createElement('h1');
-  const textoGerado = document.createTextNode(texto.value);
+  // if (!elementoPai || elementoPai.value === '') {
+  //   return elementoPai.appendChild('Por favor, digite o conteúdo da carta.');
+  // }
+  // const novoConteudo = '<span>" "</span>';
+  // const elementoSeparado = elementoPai.split(' ');
+  const titulo = document.createElement('span');
+  const textoGerado = document.createTextNode(texto.split(' '));
   const t = titulo.appendChild(textoGerado);
   console.log(t);
-  return elementoPai.appendChild(t);
+  const cartaPronta = elementoPai.appendChild(t);
+  return cartaPronta;
 });
